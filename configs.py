06873219a7efeaf55f4c88912f2b055fd0cc3b47
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-02 13:47:39 +0530

    Add files via upload

--- configs.py
+++ configs.py
@@ -2,21 +2,25 @@
 # Subscribe YouTube Channel For Amazing Bot @spidey_official_777
 # Ask Doubt on telegram @hacker_x_official_777
 
-from os import path, getenv
+from os import path, getenv, environ
 
 class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     
-    CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1001959922658,-1002470391435,-1002433552221").split(","))) # for the multiple forcesub
+    CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1001959922658,-1002470391435,-1002433552221, -1002252585703").split(",")))
     
     REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
-    # Spidey
+    
     ADMINS = list(map(int, getenv("ADMINS", "5518489725").split()))
     DATABASE_URI = getenv("DATABASE_URI", "")
 
     LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1002294764885")) 
+
+
+START_IMG = (environ.get('START_IMG', 'https://graph.org/file/2518d4eb8c88f8f669f4c.jpg https://graph.org/file/d6d9d9b8d2dc779c49572.jpg https://graph.org/file/4b04eaad1e75e13e6dc08.jpg https://graph.org/file/05066f124a4ac500f8d91.jpg https://graph.org/file/2c64ed483c8fcf2bab7dd.jpg https://i.ibb.co/CPxdkHR/IMG-20240818-192201-633.jpg')).split()
+
 
 class temp(object):    
     U_NAME = None
