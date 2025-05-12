cbab60e04deeb0e3fb3851068f0701d92a7fdbc4
ak4422920 <ak4422920@gmail.com>
2025-05-12 11:30:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@
 
 class script(object):
 
-    START_MSG = """<b>🦊 Hᴇʟʟᴏ {}!\n\nI'ᴍ ᴀɴ ᴀᴜᴛᴏ-ᴀᴘᴘʀᴏᴠᴇ [ᴀᴅᴍɪɴ](https://t.me/hacker_x_official_777) [Jᴏɪɴ Rᴇǫᴜᴇsᴛs](https://t.me/telegram/153) ʙᴏᴛ.\nI ᴄᴀɴ ᴀᴘᴘʀᴏᴠᴇ ᴜsᴇʀs ɪɴ Gʀᴏᴜᴘs/Cʜᴀᴛs. Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ᴀɴᴅ [ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴛᴏ ᴀᴅᴍɪɴ]()\n\nn<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_𝟽𝟽𝟽</a></blockquote></b>"""
+    START_MSG = """<b>🦊 Hᴇʟʟᴏ {}!\n\nI'ᴍ ᴀɴ ᴀᴜᴛᴏ-ᴀᴘᴘʀᴏᴠᴇ [ᴀᴅᴍɪɴ](https://t.me/Cineoriginals) [Jᴏɪɴ Rᴇǫᴜᴇsᴛs](https://t.me/AKMovieBotz) ʙᴏᴛ.\nI ᴄᴀɴ ᴀᴘᴘʀᴏᴠᴇ ᴜsᴇʀs ɪɴ Gʀᴏᴜᴘs/Cʜᴀᴛs. Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ᴀɴᴅ [ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴛᴏ ᴀᴅᴍɪɴ]()\n\nn<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a></blockquote></b>"""
     
     DISCLAIMER_TXT = """
 <b>      ᴀᴜᴛᴏ ʀᴇǫᴜᴇsᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ
@@ -12,7 +12,7 @@ class script(object):
    
   ᴛʜɪs ʙᴏᴛ ɪs ᴀ ᴘᴜʙʟɪᴄʟʏ ᴀᴠᴀɪʟᴀʙʟᴇ ᴛᴏᴏʟ ᴅᴇsɪɢɴᴇᴅ ᴛᴏ ᴀssɪsᴛ ᴄʜᴀɴɴᴇʟ ᴏᴡɴᴇʀs ʙʏ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛʜᴇ ᴘʀᴏᴄᴇss ᴏғ ᴀᴄᴄᴇᴘᴛɪɴɢ ʀᴇǫᴜᴇsᴛs ᴏɴ ᴛʜᴇɪʀ ᴄʜᴀɴɴᴇʟs. Iᴛ ɪs ɪɴᴛᴇɴᴅᴇᴅ ᴛᴏ ɪɴᴄʀᴇᴀsᴇ ᴇғғɪᴄɪᴇɴᴄʏ ᴀɴᴅ ᴇɴʜᴀɴᴄᴇ ᴛʜᴇ ʀᴇǫᴜᴇsᴛ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴇxᴘᴇʀɪᴇɴᴄᴇ. ᴀʟʟ ʀɪɢʜᴛs ᴛᴏ ᴛʜᴇ ᴏʀɪɢɪɴᴀʟ sᴏғᴛᴡᴀʀᴇ ᴀɴᴅ ʀᴇʟᴀᴛᴇᴅ ᴄᴏɴᴛᴇɴᴛ ᴀʀᴇ ᴛʜᴇ ᴘʀᴏᴘᴇʀᴛʏ ᴏғ ᴛʜᴇɪʀ ʀᴇsᴘᴇᴄᴛɪᴠᴇ ᴄᴏᴘʏʀɪɢʜᴛ ʜᴏʟᴅᴇʀs. Tʜɪs ʙᴏᴛ ɪs ɴᴏᴛ ᴀғғɪʟɪᴀᴛᴇᴅ ᴡɪᴛʜ, ᴇɴᴅᴏʀsᴇᴅ ʙʏ, ᴏʀ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ʙʏ ᴛᴇʟᴇɢʀᴀᴍ , ᴜɴʟᴇss ᴏᴛʜᴇʀᴡɪsᴇ sᴘᴇᴄɪғɪᴇᴅ. Usᴇ ᴏғ ᴛʜɪs ʙᴏᴛ ᴅᴏᴇs ɴᴏᴛ ɢʀᴀɴᴛ ᴏᴡɴᴇʀsʜɪᴘ ʀɪɢʜᴛs ᴛᴏ ᴛʜᴇ sᴏғᴛᴡᴀʀᴇ ᴏʀ ᴀssᴏᴄɪᴀᴛᴇᴅ ɪɴᴛᴇʟʟᴇᴄᴛᴜᴀʟ ᴘʀᴏᴘᴇʀᴛʏ. ʙʏ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ, ʏᴏᴜ ᴀᴄᴋɴᴏᴡʟᴇᴅɢᴇ ᴀɴᴅ ᴀɢʀᴇᴇ ᴛᴏ ᴄᴏᴍᴘʟʏ ᴡɪᴛʜ ᴀʟʟ ʀᴇʟᴇᴠᴀɴᴛ ᴛᴇʀᴍs ᴏғ sᴇʀᴠɪᴄᴇ ᴀɴᴅ ᴜsᴀɢᴇ ᴘᴏʟɪᴄɪᴇs ᴏғ ᴛʜᴇ ᴘʟᴀᴛғᴏʀᴍs ʏᴏᴜ ɪɴᴛᴇʀᴀᴄᴛ ᴡɪᴛʜ. ғᴏʀ ɪɴǫᴜɪʀɪᴇs ᴏʀ ᴄᴏɴᴄᴇʀɴs, ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴏʀ ʀᴇғᴇʀ ᴛᴏ ᴛʜᴇ ʀᴇʟᴇᴠᴀɴᴛ ᴘʟᴀᴛғᴏʀᴍ's ᴛᴇʀᴍs ᴏғ sᴇʀᴠɪᴄᴇ. 
 
-<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></b>
+<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a></b>
  """
     
     LOGO_MSG ="""
@@ -36,17 +36,17 @@ class script(object):
 
 ᴀɴᴅ ᴜsɪɴɢ ᴀɴᴅ <a href='https://cloud.mongodb.com'>ᴍᴏɴɢᴏ</a> ᴀs ᴅᴀᴛᴀʙᴀsᴇ.
 
-» Sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ : <a href='https://youtube.com/@spidey_official_777'>Sᴘɪᴅᴇʏ ᴏғғɪᴄɪᴀʟ</a>
+» Sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Cineoriginals'>Cineoriginals</a>
 
-ꜱᴘɪᴅᴇʏ  ɪs ʟɪᴄᴇɴsᴇᴅ ᴜɴᴅᴇʀ ᴛʜᴇ <a href='https://github.com/Spideyofficial777/Spidey-MtLisance/blob/main/LICENSE'>ᴍɪᴛ ʟɪᴄᴇɴsᴇ</a> .
+ʟɪᴄᴇɴsᴇᴅ ᴜɴᴅᴇʀ ᴛʜᴇ <a href='https://github.com'>ᴍɪᴛ ʟɪᴄᴇɴsᴇ</a> .
 
-© 𝟸𝟶𝟸𝟻 | <a href=https://t.me/SPIDEYOFFICIAL777'>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> , ᴀʟʟ ʀɪɢʜᴛs ʀᴇsᴇʀᴠᴇᴅ.
+© 𝟸𝟶𝟸𝟻 | <a href=https://t.me/+U9ABfC7hu1EyZjU1'>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> , ᴀʟʟ ʀɪɢʜᴛs ʀᴇsᴇʀᴠᴇᴅ.
   
-  <blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></blockquote></b>"""
+  <blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a></blockquote></b>"""
     
     ALERT_MSG = """ ⚡️ ʟɪᴠᴇ sʏsᴛᴇᴍ sᴛᴀᴛᴜs ⚡️\n\n❂ ʀᴀᴍ ●●●●●●●◌◌◌\n✇ ᴄᴘᴜ ●●●●●●●◌◌◌\n✪ ᴅᴀᴛᴀ ᴛʀᴀꜰɪᴄs ●●●●◌◌◌◌◌◌ 🛰\n\nᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ] """
     
-    ABOUT_TXT = """<blockquote><b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://telegram.me/SPIDER_MAN_GAMING_bot'>sᴘɪᴅᴇʏ_ɢᴀᴍɪɴɢ_ᴏғғɪᴄɪᴀʟ_ʙᴏᴛ</a>\n‣ ᴄʀᴇᴀᴛᴏʀ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a>\n‣ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ\n‣ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ\n‣ ᴅᴀᴛᴀ ʙᴀsᴇ : ᴍᴏɴɢᴏ ᴅʙ\n‣ ʜᴏsᴛᴇᴅ ᴏɴ  : ᴀʟʟ ᴡᴇʙ\n‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ]</b></blockquote>"""    
+    ABOUT_TXT = """<blockquote><b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://telegram.me/Autoaprrovejoinacceptbot'>Autoaprrovejoinacceptbot</a>\n‣ ᴄʀᴇᴀᴛᴏʀ : <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a>\n‣ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ\n‣ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ\n‣ ᴅᴀᴛᴀ ʙᴀsᴇ : ᴍᴏɴɢᴏ ᴅʙ\n‣ ʜᴏsᴛᴇᴅ ᴏɴ  : ᴀʟʟ ᴡᴇʙ\n‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ]</b></blockquote>"""    
     
     CHANNELS = """
 <b>๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ᴛᴏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ɢᴇᴛ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴜs.</b>"""
@@ -58,9 +58,9 @@ class script(object):
     OWNER_TEXT = """
 <b>━━━━━━━━━━━━━━━━━━━━━
 🚀 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀɴ ᴇʟɪᴛᴇ ᴅᴇᴠ 🚀  
-👑 ᴏᴡɴᴇʀ:  <a href='https://telegram.me/ʜacker_x_official_777'>ꜱᴘɪᴅᴇʏ</a>
-🔗 ᴛᴇʟᴇɢʀᴀᴍ: <a href='https://telegram.me/spideyofficial777'>ꜱᴘɪᴅᴇʏᴏғғɪᴄɪᴀʟ𝟽𝟽𝟽</a>  
-🌍 ɢɪᴛʜᴜʙ: <a href='https://telegram.me/Hacker_x_official_777'>ꜱᴘɪᴅᴇʏ ᴏғғɪᴄɪᴀʟ</a>
+👑 ᴏᴡɴᴇʀ:  <a href='https://telegram.me/The_SonGoku'>The_SonGoku</a>
+🔗 ᴛᴇʟᴇɢʀᴀᴍ: <a href='https://telegram.me/AKMovieBotz'>AkMoviesHubBackup</a>  
+🌍 ɢɪᴛʜᴜʙ: <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a>
 ━━━━━━━━━━━━━━━━━━━━━
 🔥 ɪɴɴᴏᴠᴀᴛɪɴɢ ᴛʜᴇ ғᴜᴛᴜʀᴇ, ɴᴇ ᴄᴏᴅᴇ ᴀᴛ ᴀ ᴛɪᴍᴇ! 🔥
 ━━━━━━━━━━━━━━━━━━━━━</b>
@@ -78,7 +78,7 @@ class script(object):
 <b>🕐 ᴊᴏɪɴᴇᴅ ᴀᴛ :</b> <code>{time}</code>
 """
 
-    GSTART_TXT = """<b>ʜᴇʏ {},\n\nɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ʀᴇǫᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ.\n\nᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ꜱᴘɪᴅᴇʏ</a></b>"""
+    GSTART_TXT = """<b>ʜᴇʏ {},\n\nɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ʀᴇǫᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ.\n\nᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/AKMovieBotz'>AKMovieBotz</a></b>"""
 
 
 # Don't Remove Credit @spideyofficial777
